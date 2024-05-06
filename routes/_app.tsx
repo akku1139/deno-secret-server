5d4bc6d5ec869fb0189009e99b811f7acd95bc80
akkun11.open <akkun11.open@gmail.com>
2024-05-06 13:07:14 +0000

    mini

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,11 +1,12 @@
 import { type PageProps } from "$fresh/server.ts";
+
 export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>my-fresh-app</title>
+        <title>Deno Secret Server</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
